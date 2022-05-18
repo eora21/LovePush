14051f3e3a68bef1d12618af36b34dd00f43cdf3
eora21 <eora21@naver.com>
2022-05-18 10:31:50 +0900

    refactor: 💡 git merge
    머지했습니다.

---
+++ backend/joalarm/src/main/java/com/cupid/joalarm/redis/publisher/RedisPublisher.java
@@ -0,0 +1,18 @@
+package com.cupid.joalarm.redis.publisher;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class RedisPublisher {
+    private final RedisTemplate<String,Object> redisTemplate;
+
+    public void publish(ChannelTopic topic, ChatDTO chatDTO){
+        redisTemplate.convertAndSend(topic.getTopic(),chatDTO);
+    }
+
+}
